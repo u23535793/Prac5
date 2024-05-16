38675b0a1fe6d80f16a5b8a82eacea3039f611b6
Michelle Njorge <micheyn83uni@gmail.com>
2024-05-16 21:06:07 +0200

    This is my very detailed commit message-Michelle

--- quarterStack_testing.java/src/App.java
+++ quarterStack_testing.java/src/App.java
@@ -1,5 +1,7 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Hoopers");
+        System.out.println("This is Michelle :)");
+
     }
 }
