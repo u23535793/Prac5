a525903530323bf7e4b2d56ab0947d0b845a3f0f
boitymtsatse <boitymtsatse@gmail.com>
2024-05-16 21:08:55 +0200

    Boity's first commit

--- quarterStack_testing.java/src/App.java
+++ quarterStack_testing.java/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello Hoopers");
         System.out.println("This is a message from Driya");
+        System.out.println("Merry Christmas");
     }
 }
